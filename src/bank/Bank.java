3a06723b5cfc5a5363472ace0f2a5b4c41098b5c
Evan Zhong <zelinzhong@wustl.edu>
2022-04-04 13:55:29 -0500

    changed

--- src/bank/Bank.java
+++ src/bank/Bank.java
@@ -1,13 +1,14 @@
 package bank;
 import java.util.LinkedList;
 
-import bank.Account1;
+import bank.Account;
+import bank.Users;
 
 public class Bank {
 	
 	public String name;
 	
-	private LinkedList<Account1> accounts;
+	private LinkedList<Users> users;
 
 	
 	public static void main(String[] args) {
--- src/bank/Users.java
+++ src/bank/Users.java
@@ -1,7 +1,12 @@
 package bank;
+import java.util.LinkedList;
+
 import bank.Account;
 
 public class Users {
+	public String name;
+	private String password;
+	private LinkedList<Account> accounts;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
