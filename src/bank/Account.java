d35e860f44d4b96bb23b797216cecf665b3eccfa
Evan Zhong <zelinzhong@wustl.edu>
2022-04-04 22:59:24 -0500

    done with open account

--- src/bank/Account.java
+++ src/bank/Account.java
@@ -1,13 +1,88 @@
 package bank;
 
+import java.util.Random;
+
 public class Account {
 	private String accountType;
-	private int accountNumber;
+	private String accountNumber;
 	private String user;
 	private long balance;
+	
+	public static String generateAccountNumber() {
+		Random random = new Random();
+		char[] digits = new char[8];
+		for (int i = 0; i < 8; i++) {
+			digits[i] = (char) (random.nextInt(10) + '0');
+		}
+		return new String(digits);
+	}  
+	
+	public static boolean checkType(String type) {
+		if(!(type.equalsIgnoreCase("saving")||type.equalsIgnoreCase("checking"))) {
+			return false;
+		}
+		return true;
+	}
+	public static String updateType(String type) {
+		if(type.equalsIgnoreCase("saving")){
+			String s="saving";
+			return s;
+			}
+		else if(type.equalsIgnoreCase("checking")) {
+			String s="checking";
+			return s;
+		}
+		else {
+			throw new IllegalArgumentException("invalid account type, can only be saving and checking");
+		}
+			
+	}
+	public Account(String name, String type) {
+		if(checkType(type)) {
+			this.user=name;
+			this.balance=0;
+			this.accountNumber=generateAccountNumber();
+			this.accountType=updateType(type);
+		}
+		else {
+			throw new IllegalArgumentException("invalid account type, can only be saving and checking");
+		}
+	}
+	
+	public Account(String name,String type, long balance) {
+		if(checkType(type)) {
+			this.user=name;
+			this.balance=balance;
+			this.accountNumber=generateAccountNumber();
+			this.accountType=updateType(type);
+		}
+		else {
+			throw new IllegalArgumentException("invalid account type, can only be saving and checking");
+		}
+		
+	}
+	
+	public String getAccountNumber() {
+		return this.accountNumber;
+	}
+	
+	public String getUser() {
+		return this.user;
+	}
+	
+	public long getBalance() {
+		return this.balance;
+	}
+	
+	public String getAccountType() {
+		return this.accountType;
+	}
+	
 
 	public static void main(String[] args) {
 		// -generated method stub
+		Account accountOne=new Account("Evan","checking");
+		System.out.print(String.valueOf(accountOne.getAccountNumber()));
 
 	}
 
---
+++ src/test/accountTest.java
@@ -0,0 +1,62 @@
+package test;
+import bank.Account;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class accountTest {
+
+	@Test
+	void testExpectedExceptionOne() {
+		assertThrows(IllegalArgumentException.class,()->{
+			Account account=new Account("Jason","chec");
+		});
+		assertThrows(IllegalArgumentException.class,()->{
+			Account account=new Account("Jason","sav",100);
+		});
+		
+	}
+	@Test
+	void testExpectedExceptionNotThrow() {
+		try {
+			Account accountOne=new Account("Evan","Checking");
+			Account accountTwo=new Account("Logan","Saving",100);
+		}
+		catch(Exception e) {
+			fail("Should have not thrown any exception");
+		}
+	}
+	@Test
+	void testGetUser() {
+		Account accountOne=new Account("Evan","Checking");
+		assertEquals("Evan",accountOne.getUser());
+		Account accountTwo=new Account("Logan","Saving",100);
+		assertEquals("Logan",accountTwo.getUser());
+		
+	}
+	@Test
+	void testGetBalance() {
+		Account accountOne=new Account("Evan","Checking");
+		assertEquals(0,accountOne.getBalance());
+		Account accountTwo=new Account("Logan","Saving",100);
+		assertEquals(100,accountTwo.getBalance());
+	}
+	@Test
+	void testGetAccountType() {
+		Account accountOne=new Account("Evan","Checking");
+		assertEquals("checking",accountOne.getAccountType());
+		Account accountTwo=new Account("Logan","Saving",100);
+		assertEquals("saving",accountTwo.getAccountType());	
+	}
+	@Test
+	void testGetAccountNumber() {
+		Account accountOne=new Account("Evan","Checking");
+		String numOne=accountOne.getAccountNumber();
+		assertEquals(8,numOne.length());
+		Account accountTwo=new Account("Logan","Saving",100);
+		String numTwo=accountTwo.getAccountNumber();
+		assertEquals(8,numTwo.length());
+	}
+
+}
